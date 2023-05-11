1965910e994d5d6c76de449a5485e8d64c49cd96
Aaron Fleck <revolverwest@gmail.com>
2023-05-11 13:17:52 -0600

    Update Update_Adobe-AUM.ps1
    rework to allow placing RUM automatically.

--- Update_Adobe-AUM.ps1
+++ Update_Adobe-AUM.ps1
@@ -1,34 +1,41 @@
 <#
 .Description
-
-Takes Tenable CSV export and sends updates using the Adobe Update Manager (if installed).
+Run Remove Update Manager from CSV list exported from Tenable. Will detect Remote Update Manager executable, or copy the files to the proper destination if not found. Will output a log, IP addresses for Tenable remediation scan, and list of computers which were not able to be reached.
 
 .Notes
-This enters a PS Session, admin credentials may be required. The PSSession is closed upon exit.
+This enters a PS Session, administrator rights for the remote computer will be required. The PSSession is closed upon exit. Default config has the CSV, logs directory, and RemoteUpdateManager with files in the Documents folder.
 
 .Link
 https://helpx.adobe.com/enterprise/using/using-remote-update-manager.html
 #>
 
+$path = 'C:\Users\ADMIN\Documents'
 $Date = Get-Date -UFormat "%Y%m%d"
-Start-Transcript -Path logs\$Date-Adobe_Update.txt -Append
+Start-Transcript -Path $path\logs\$Date-Adobe_Update.txt -Append
 # Pull list of names from a docuemnt
-$Collection = Import-CSV .\Tenable.csv
+$Collection = Import-CSV "$path\Tenable.csv"
 
 # Option to use an admin account
 #$sesh = Get-Credential
 
-Foreach ($item in $Collection."asset.name") {
+Foreach ($item in $Collection.'asset.name') {
 
     # test to see if computer in list is online
     $online = Test-Path "\\$item\c$\Program Files (x86)\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager\"
+    $NeedRUM = Test-Path "\\$item\c$\"
     if ($online) {
         # Open a remote session on the named computer and run the update.
         Invoke-Command -ComputerName $item -scriptblock {
         & 'C:\Program Files (x86)\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager\RemoteUpdateManager.exe' --action=install
         }
-        Resolve-DnsName $item | Select-Object -ExpandProperty IPaddress | Out-File -FilePath .\$date_Adobe_rescan.txt -Append
-    } else {
+        Resolve-DnsName $item | Select-Object -ExpandProperty IPaddress | Out-File -FilePath "$path\$date-Adobe_rescan.txt" -Append
+    } elseif ($NeedRUM) {
+        Robocopy.exe "$path\RemoteUpdateManager" "\\$item\c$\Program Files (x86)\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager" /MT /NP
+        Invoke-Command -ComputerName $item -scriptblock {
+            & 'C:\Program Files (x86)\Common Files\Adobe\OOBE_Enterprise\RemoteUpdateManager\RemoteUpdateManager.exe' --action=install
+        }
+        Resolve-DnsName $item | Select-Object -ExpandProperty IPaddress | Out-File -FilePath "$path\$date-Adobe_rescan.txt" -Append
+    } Else {
             Write-Warning "$item is not reachable."
     }
 }
